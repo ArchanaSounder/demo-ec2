685efa4ce4a576bab214484262657b4147dbd2b2
ArchanaSounder <81344540+ArchanaSounder@users.noreply.github.com>
2024-04-30 15:03:48 +0530

    Update ec2-demo.tf

--- ec2-demo.tf
+++ ec2-demo.tf
@@ -3,7 +3,7 @@ module "aws-vm" {
   version                     = "1.2.0"
   ami_ssm_parameter           = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
   associate_public_ip_address = var.associate_public_ip_address
-  instance_type               = var.instance_type
+  instance_type               = var.aws_instance_type
   key_name                    = var.key_name      # Existing key name
   subnet_id                   = var.subnet_id 
   tags                        = var.tags
